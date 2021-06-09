49d1de73797a66d350caaebd55875bf3a3acd88e
ESTIBEN GIRALDO <estiben.giraldo@tismart.com>
2021-06-09 17:13:56 -0500

    DISEÑO LOGIN

--- app/src/main/java/com/tawa/allinapp/core/di/ApplicationComponent.kt
+++ app/src/main/java/com/tawa/allinapp/core/di/ApplicationComponent.kt
@@ -2,12 +2,8 @@ package com.tawa.allinapp.core.di
 
 import com.tawa.allinapp.AndroidApplication
 import com.tawa.allinapp.core.di.viewmodel.ViewModelModule
-import com.tawa.allinapp.features.HomeActivity
-import com.tawa.allinapp.features.calendar.CalendarFragment
-import com.tawa.allinapp.features.catalog.CatalogFragment
-import com.tawa.allinapp.features.location.LocationFragment
+import com.tawa.allinapp.features.auth.ui.LoginFragment
 import com.tawa.allinapp.features.movies.ui.MoviesFragment
-import com.tawa.allinapp.features.reports.ReportsFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -15,10 +11,7 @@ import javax.inject.Singleton
 @Component(modules = [ApplicationModule::class, ViewModelModule::class])
 interface ApplicationComponent{
     fun inject(application: AndroidApplication)
-    fun inject(homeActivity: HomeActivity)
     fun inject(moviesFragment: MoviesFragment)
-    fun inject(locationFragment: LocationFragment)
-    fun inject(catalogFragment: CatalogFragment)
-    fun inject(calendarFragment: CalendarFragment)
-    fun inject(reportsFragment: ReportsFragment)
+    fun inject(loginFragment: LoginFragment)
+
 }
