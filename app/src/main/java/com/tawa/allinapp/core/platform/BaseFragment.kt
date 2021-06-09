770d13f2fa19bfe127ef5307ce6e7b2cd1dae981
Rodrigo <rvaldiviamalaga@gmail.com>
2021-06-09 18:19:02 -0500

    main menu, navigation and geo

--- app/src/main/java/com/tawa/allinapp/core/platform/BaseFragment.kt
+++ app/src/main/java/com/tawa/allinapp/core/platform/BaseFragment.kt
@@ -8,6 +8,7 @@ import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
 import com.tawa.allinapp.R
 import com.google.android.material.snackbar.Snackbar
 import com.tawa.allinapp.AndroidApplication
@@ -19,17 +20,11 @@ import javax.inject.Inject
 
 abstract class BaseFragment : Fragment() {
 
-    abstract fun layoutId(): Int
-
     val appComponent: ApplicationComponent by lazy(mode = LazyThreadSafetyMode.NONE) {
         (activity?.application as AndroidApplication).appComponent
     }
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-        inflater.inflate(layoutId(), container, false)
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     open fun onBackPressed() {}
 
