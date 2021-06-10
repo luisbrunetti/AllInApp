6d0bf8c612c80e7de2aa3900c95517d009320fd1
ESTIBEN GIRALDO <estiben.giraldo@tismart.com>
2021-06-10 16:27:51 -0500

    DISEÑO LOGIN 1

--- app/src/main/java/com/tawa/allinapp/features/auth/ui/LoginFragment.kt
+++ app/src/main/java/com/tawa/allinapp/features/auth/ui/LoginFragment.kt
@@ -1,24 +1,16 @@
 package com.tawa.allinapp.features.auth.ui
 
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
-import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnScrollChangedListener
 import android.widget.Toast
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.tawa.allinapp.R
-import com.tawa.allinapp.core.extensions.failure
-import com.tawa.allinapp.core.extensions.observe
-import com.tawa.allinapp.core.extensions.viewModel
 import com.tawa.allinapp.core.platform.BaseFragment
-import com.tawa.allinapp.databinding.FragmentMoviesBinding
 import com.tawa.allinapp.databinding.LoginFragmentBinding
-import com.tawa.allinapp.features.movies.MoviesViewModel
 import kotlinx.android.synthetic.main.login_fragment.*
-import javax.inject.Inject
 
 
 class LoginFragment : BaseFragment() {
@@ -49,13 +41,17 @@ class LoginFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        mBinding.emailText.clearFocus()
+
 
         mBinding.button.setOnClickListener(View.OnClickListener {
 
             Toast.makeText(context,"Login",Toast.LENGTH_SHORT).show()
 
         })
+
+
+
+
 
 
        // mBinding.layoutPassword.setPasswordVisibilityToggleDrawable(R.drawable.ic_vi)
