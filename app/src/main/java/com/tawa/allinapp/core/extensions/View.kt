257daf99116cbfb6106d1fb00eb6988e2bd97e4c
Rodrigo <rvaldiviamalaga@gmail.com>
2021-06-16 17:20:06 -0500

    View.setButtonEnabled

--- app/src/main/java/com/tawa/allinapp/core/extensions/View.kt
+++ app/src/main/java/com/tawa/allinapp/core/extensions/View.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
+import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
@@ -20,3 +21,16 @@ fun ImageView.loadFromUrl(url: String) =
 fun View.visible() { this.visibility = View.VISIBLE }
 
 fun View.invisible() { this.visibility = View.GONE }
+
+@BindingAdapter("enableButton")
+fun View.setButtonEnabled(value: Boolean?){
+    value?.let { enable ->
+        if (enable){
+            isEnabled = true
+            alpha = 1.0F
+        }else{
+            isEnabled = false
+            alpha = 0.5F
+        }
+    }
+}
