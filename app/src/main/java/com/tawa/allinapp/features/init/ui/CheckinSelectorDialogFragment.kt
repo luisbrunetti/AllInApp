e9aad4db69fc67c367fc42a1c1fb5a47aed911d7
ESTIBEN GIRALDO <estiben.giraldo@tismart.com>
2021-06-18 18:46:06 -0500

    MOSTRAR HORA Y DIA

--- app/src/main/java/com/tawa/allinapp/features/init/ui/CheckinSelectorDialogFragment.kt
+++ app/src/main/java/com/tawa/allinapp/features/init/ui/CheckinSelectorDialogFragment.kt
@@ -1,4 +1,4 @@
-package com.tawa.allinapp.features.init.ui
+package com.tawa.allinapp.core.dialog
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
@@ -14,8 +16,9 @@ import com.tawa.allinapp.core.extensions.observe
 import com.tawa.allinapp.core.extensions.viewModel
 import com.tawa.allinapp.core.platform.BaseFragment
 import com.tawa.allinapp.databinding.DialogCheckinBinding
-import com.tawa.allinapp.models.Company
-import com.tawa.allinapp.models.PV
+import com.tawa.allinapp.databinding.DialogHomeBinding
+import com.tawa.allinapp.features.auth.Company
+import com.tawa.allinapp.features.auth.PV
 import com.tawa.allinapp.features.init.InitViewModel
 import javax.inject.Inject
 
@@ -25,32 +28,64 @@ class CheckinSelectorDialogFragment
     private val baseFragment: BaseFragment
 ): DialogFragment() {
 
+
+
+
     private lateinit var binding: DialogCheckinBinding
     private  lateinit var initViewModel: InitViewModel
+    private val arrayListPv:ArrayList<String> = ArrayList<String>()
+
+
+
+
     var listener: Callback? = null
+
+
+
+    companion object {
+
+
+    }
+
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DialogCheckinBinding.inflate(inflater)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         isCancelable = false
-        val arrayListPv:ArrayList<String> = ArrayList<String>()
+
+
         val  aaPv = ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_dropdown_item, arrayListPv)
+        //val arrayList:ArrayList<String> = ArrayList<String>()
+
+       // val  aa = ArrayAdapter<String>(requireActivity(), android.R.layout.simple_spinner_dropdown_item, arrayList)
 
         initViewModel = viewModel(baseFragment.viewModelFactory){
+
             observe(startCheckIn, {
                 it?.let {
+
                     if(it) {
                         //getCompanies()
                         getPv()
+
                     }
+
                 }
             })
+
+
             observe(pv, {
                 it?.let {
                     arrayListPv.addAll(toArrayPv(it))
                     binding.pdvSpinner.adapter = aaPv
                 }
             })
+
+
+
         }
 
         return binding.root
@@ -58,10 +93,13 @@ class CheckinSelectorDialogFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.btnDoCheckin.setOnClickListener {
             listener?.onAccept()
             dismiss()
-            Toast.makeText(context,"Guardar checkin",Toast.LENGTH_SHORT).show()
+            val posi  = binding.pdvSpinner.selectedItemPosition
+
+            Toast.makeText(context,""+posi,Toast.LENGTH_SHORT).show()
         }
     }
 
