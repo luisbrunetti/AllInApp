da8a558ad900bba2fc2135282909c0c18159f822
ESTIBEN GIRALDO <estiben.giraldo@tismart.com>
2021-06-25 18:42:46 -0500

    REPORTES CABECERA Y SELECCIONAR FECHA

--- app/src/main/java/com/tawa/allinapp/features/reports/ReportsFragment.kt
+++ app/src/main/java/com/tawa/allinapp/features/reports/ReportsFragment.kt
@@ -1,13 +1,20 @@
 package com.tawa.allinapp.features.reports
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.tawa.allinapp.core.extensions.observe
 import com.tawa.allinapp.core.extensions.viewModel
 import com.tawa.allinapp.core.platform.BaseFragment
 import com.tawa.allinapp.databinding.FragmentReportsBinding
+import kotlinx.android.synthetic.main.fragment_reports.*
+import java.text.SimpleDateFormat
+import java.time.Month
+import java.util.*
 
 
 class ReportsFragment : BaseFragment() {
@@ -25,10 +32,43 @@ class ReportsFragment : BaseFragment() {
         reportsViewModel = viewModel(viewModelFactory) {
             observe(text, {
                 it?.let {
-                    binding.tvReports.text = it
+
                 }
             })
         }
+
+        binding.textDate.setOnClickListener{
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+            val dpd = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                binding.textDate.setText("" + dayOfMonth + "/" +getMonth(monthOfYear) + "/" + year) }, year, month, day)
+            dpd.show()
+        }
         return binding.root
     }
+
+    private fun getMonth(monthYear: Int) = when(monthYear){
+
+            0 -> "01"
+            1 -> "02"
+            2 -> "03"
+            3 -> "04"
+            4 -> "05"
+            5 -> "06"
+            6 -> "07"
+            7 -> "08"
+            8 -> "09"
+            9 -> "10"
+            10 -> "11"
+            11 -> "12"
+        else  ->""
+    }
+
+
+
+
 }
