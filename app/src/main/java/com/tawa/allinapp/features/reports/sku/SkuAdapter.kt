5819315e5cbad121f355af897b9523b54bb916f8
Rodrigo <rvaldiviamalaga@gmail.com>
2021-07-12 09:53:58 -0500

    table sku

--- app/src/main/java/com/tawa/allinapp/features/reports/sku/SkuAdapter.kt
+++ app/src/main/java/com/tawa/allinapp/features/reports/sku/SkuAdapter.kt
@@ -5,22 +5,32 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tawa.allinapp.databinding.RowSkuBinding
+import com.tawa.allinapp.databinding.RowSkuHeaderBinding
 import com.tawa.allinapp.models.Sku
 import javax.inject.Inject
 
 
 class SkuAdapter
-@Inject constructor(val context: Context) : RecyclerView.Adapter<SkuAdapter.SkuViewHolder>() {
+@Inject constructor(val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private val HEADER = 1
     var collection = ArrayList<Sku>()
 
     internal var clickListener: (Sku) -> Unit = { _ -> }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        SkuViewHolder.from(parent)
+        when(viewType){
+            HEADER -> SkuViewHolderHeader.from(parent)
+            else -> SkuViewHolder.from(parent)
+        }
 
-    override fun onBindViewHolder(viewHolder: SkuViewHolder, position: Int) =
-        viewHolder.bind(context, collection[position], clickListener)
+    override fun getItemViewType(position: Int) = if(position==0) HEADER else 0
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) =
+        when(holder.itemViewType){
+            HEADER -> (holder as SkuViewHolderHeader).bind(context, collection[position], clickListener)
+            else -> (holder as SkuViewHolder).bind(context, collection[position], clickListener)
+        }
 
     override fun getItemCount() = collection.size
 
@@ -46,6 +56,26 @@ class SkuAdapter
                 val inflater = LayoutInflater.from(parent.context)
                 val binding = RowSkuBinding.inflate(inflater,parent,false)
                 return SkuViewHolder(binding)
+            }
+        }
+    }
+
+    class SkuViewHolderHeader(val binding : RowSkuHeaderBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(context: Context, item: Sku, clickListener: (Sku) -> Unit) {
+            binding.tvProduct.text = "Producto"
+            binding.tvDate.text = "Fecha Data"
+            binding.tvCategory.text = "Categoria"
+            binding.tvSubCategory.text = "Subcategoría"
+            binding.tvInventory.text = "Inventario"
+            binding.tvPrice.text = "Precio"
+        }
+
+        companion object{
+            fun from(parent : ViewGroup) : SkuViewHolderHeader {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = RowSkuHeaderBinding.inflate(inflater,parent,false)
+                return SkuViewHolderHeader(binding)
             }
         }
     }
